b883c18328f539b3c40da7c46cca8e9ce3c938cc
gtfoghost <59762963+gtfovamp@users.noreply.github.com>
2022-10-03 20:13:07 +0400

    123

--- Fetch/Fetch.py
+++ Fetch/Fetch.py
@@ -1,12 +1,13 @@
 import base64
+import shutil
 import urllib
 
+import requests
 import telebot
 from PIL import Image
 from craiyon import Craiyon
 from dalle2 import Dalle2
-from telebot import types
-from Constants.constansts import *
+from Resources.Constants.ConstantMessages import *
 from Utill.UrlExtractor import *
 
 bot = telebot.TeleBot(API_KEY)
@@ -21,18 +22,22 @@ def premiumFetch(text, message):
     urls = extractURLS(generations)
 
     list = []
-    for i in urls:
-        with urllib.request.urlopen(i) as url:
-            img = Image.open(url)
+
+    for link in urls:
+        print(link)
+        res = requests.get(link, stream=True)
+        if res.status_code == 200:
+            with open(f"{link}.webp", 'wb') as f:
+                shutil.copyfileobj(res.raw, f)
+            print('Image sucessfully Downloaded: ', "temp.webp")
+            img = open(f"{link}.webp", "rb")
             image = telebot.types.InputMediaPhoto(img)
             list.append(image)
-
-
+        else:
+            print('Image Couldn\'t be retrieved')
 
     bot.send_media_group(message.chat.id, list)
-
     bot.send_message(message.chat.id, AFTER_RESULT)
-
 
 
 def freeFetch(text, message):
@@ -46,15 +51,8 @@ def freeFetch(text, message):
         image = telebot.types.InputMediaPhoto(base64.decodebytes(i.encode("utf-8")))
         list.append(image)
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    credits = types.KeyboardButton(MY_CREDITS)
-    buy_credits = types.KeyboardButton(BUY_CREDITS)
-    requests = types.KeyboardButton(EXAMPLES_PROMTS)
-    support = types.KeyboardButton(SUPPORT)
-    markup.add(credits, buy_credits, requests, support)
-
     bot.send_media_group(message.chat.id, list)
-    bot.send_message(message.chat.id, AFTER_RESULT, reply_markup=markup)
+    bot.send_message(message.chat.id, AFTER_RESULT)
 
     result.save_images()  # Saves the generated images to 'current working directory/generated', you can also provide a custom path
 
